085ef93e26a170cb1f133f13bcec7574e321ed03
Ankit Chaudhary <achaudhary@stumbleupon.com>
2017-05-10 09:40:13 -0700

    fixed metric name resolver; added test for it

---
+++ src/test/scala/mix/kafka/monitor/LibratoOffsetReporterSpec.scala
@@ -0,0 +1,33 @@
+package mix.kafka.monitor
+
+import com.quantifind.kafka.OffsetGetter.OffsetInfo
+import com.twitter.util.Time
+import org.scalatest.{FlatSpec, Matchers}
+
+class LibratoOffsetReporterSpec extends FlatSpec with Matchers {
+
+  behavior of "getMetricName"
+
+  it should "resolve metric name in expected order" in {
+    val group = "dummy-group"
+    val topic = "dummy-topic"
+    val partition = 1
+    val offsetInfo = OffsetInfo(group, topic, partition, 0L, 0L, None, Time.epoch, Time.epoch)
+
+    val expectedMetricName = s"$group.$topic.$partition.lag"
+    LibratoOffsetReporter.getMetricName(offsetInfo) shouldBe expectedMetricName
+  }
+
+  it should "replace dots with dashes in metric name" in {
+    val group = "dummy.group"
+    val topic = "dummy.topic"
+    val resolvedGroup = "dummy-group"
+    val resolvedTopic = "dummy-topic"
+    val partition = 1
+    val offsetInfo = OffsetInfo(group, topic, partition, 0L, 0L, None, Time.epoch, Time.epoch)
+
+    val expectedMetricName = s"$resolvedGroup.$resolvedTopic.$partition.lag"
+    LibratoOffsetReporter.getMetricName(offsetInfo) shouldBe expectedMetricName
+  }
+
+}
