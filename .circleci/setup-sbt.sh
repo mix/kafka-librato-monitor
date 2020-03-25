ef085190f188c25ac8c4b00e579b741a0073b2d4
Roman Useinov <roman.useinov@gmail.com>
2020-03-25 11:03:14 +0100

    one more workaroud

--- .circleci/setup-sbt.sh
+++ .circleci/setup-sbt.sh
@@ -4,3 +4,4 @@ mkdir -p ~/.sbt/1.0/plugins
 echo -e "[repositories]\n  mix-ivy-releases: https://mix.artifactoryonline.com/mix/ivy-releases/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]\n  mix-maven-releases: https://mix.artifactoryonline.com/mix/mvn-releases/" > ~/.sbt/repositories
 echo -e "realm=Artifactory Realm\nhost=mix.artifactoryonline.com\nuser=${ARTIFACTORY_DEPLOY_USER}\npassword=${ARTIFACTORY_DEPLOY_PWD}" > ~/.sbt/.credentials
 echo "credentials += Credentials(Path.userHome / \".sbt\" / \".credentials\")" > ~/.sbt/1.0/plugins/credentials.sbt
+echo "-Dpackaging.type=jar" > ~/.sbtopts
--- build.sbt
+++ build.sbt
@@ -7,11 +7,10 @@ scalaVersion := "2.11.11"
 // Dependencies
 libraryDependencies ++= Seq(
   "com.github.ben-manes.caffeine" % "caffeine" % "1.0.0",
+  "org.reflections" % "reflections" % "0.9.11",
   "com.quantifind" %% "kafkaoffsetmonitor" % "0.4.6-SNAPSHOT" exclude("org.reflections", "reflections"),
   "com.librato.metrics" % "metrics-librato" % "5.0.5",
   "org.scalatest" %% "scalatest" % "3.0.1" % "test",
-  "org.reflections" % "reflections" % "0.9.11"
-
 )
 
 // To remove multiple exclusions
